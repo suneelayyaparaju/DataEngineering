ce094fd68769dc770e8047a44e4963be33b72a27
Suneel Ayyaparaju <Suneel.Ayyaparaju@dataqueue.com>
2019-05-23 14:34:48 +0530

    cleanup working directories

--- log-parsing/src/main/scala/com/dataqueue/LogRecord.scala
+++
@@ -1,7 +0,0 @@
-package com.dataqueue
-/**
-  * Data Model for log record.
-  *
-  * @author Suneel Ayyaparaju
-  */
-case class LogRecord( host: String, timeStamp: String, url:String, httpCode:Int)
